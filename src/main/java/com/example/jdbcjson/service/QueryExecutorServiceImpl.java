1e82bfa15b13a469aa1441255ed18804eef84c9f
Bilal Ahmad <bilalsulehri@gmail.com>
2025-08-10 13:01:48 +0500

    Unused file

--- src/main/java/com/example/jdbcjson/service/QueryExecutorServiceImpl.java
+++ src/main/java/com/example/jdbcjson/service/QueryExecutorServiceImpl.java
@@ -4,35 +4,30 @@ import com.example.jdbcjson.util.ResultSetToJsonMapper;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Service;
+
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-@Service
+
 public class QueryExecutorServiceImpl implements QueryExecutorService {
 
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    private final JdbcTemplate jdbcTemplate;
-
-    public QueryExecutorServiceImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
-    }
+   
 
     @Override
     public JsonNode executeQuery(String sql) {
-        return executeQuery(sql, Collections.emptyMap());
+       return null;
     }
 
     @Override
     public JsonNode executeQuery(String sql, Map<String, Object> parameters) {
-        return namedParameterJdbcTemplate.query(sql, parameters, ResultSetToJsonMapper::mapResultSetToJson);
+        return null;
     }
 
     @Override
     public List<String> getQueryMetadata(String sql) {
-        return jdbcTemplate.query(sql, ResultSetToJsonMapper::extractColumnMetadata);
+        return null;
     }
+
 }
